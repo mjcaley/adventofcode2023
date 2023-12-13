56730e49fd33d274b7a4adfc319b7845babf3767
Michael Caley <mjcaley@darkarctic.com>
2023-12-12 21:53:04 -0500

    Completed day 4 part 1

--- aoc/main.py
+++ aoc/main.py
@@ -2,12 +2,14 @@ import typer
 import day1
 import day2
 import day3
+import day4
 
 
 app = typer.Typer()
 app.add_typer(day1.app, name="day1")
 app.add_typer(day2.app, name="day2")
 app.add_typer(day3.app, name="day3")
+app.add_typer(day4.app, name="day4")
 
 
 if __name__ == "__main__":
